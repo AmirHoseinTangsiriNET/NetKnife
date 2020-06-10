6dab48c2776b0062a3ade293c992166b7ddbe2c5
AmirHoseinTnagsiriNET <64230943+AmirHoseinTnagsiriNET@users.noreply.github.com>
2020-06-10 17:15:42 +0430

    Update NetKnife.py

--- NetKnife.py
+++ NetKnife.py
@@ -12,10 +12,10 @@ print Fore.RED + ("""
 """)
 print Fore.GREEN + ("Hello Welcome To The NetKnife. Please Chose The Tools Number ")
 print ("[+]Chode Tools Number ")
-print ("[1]:Arp Cache Poisiner Tools")
-print ("[2]:Fake Access Point Creator Tools")
+print ("[1]:Arp Cache Poisoner Tools")
+print ("[2]:Fake Access Point Generator Tools")
 print ("[3]:SSID Hidden Locator Tools")
-print ("[4]:Hash Creator Tools")
+print ("[4]:Hash Generator Tools")
 print ("[5]:SYN Flooder Tools")
 TN = input ("[+]Please Enter the Toools Number: ")
 if TN == 1:
