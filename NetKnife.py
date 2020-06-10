236caf89ad470af61e14e87e08093d4752fb11b0
AmirHoseinTnagsiriNET <64230943+AmirHoseinTnagsiriNET@users.noreply.github.com>
2020-06-10 17:25:44 +0430

    Update NetKnife.py

--- NetKnife.py
+++ NetKnife.py
@@ -11,19 +11,21 @@ print Fore.RED + ("""
 
 """)
 print Fore.GREEN + ("Hello Welcome To The NetKnife. Please Chose The Tools Number ")
-print ("[+]Chode Tools Number ")
-print ("[1]:Arp Cache Poisoner Tools")
+print ("[+]Chose Tools Number ")
+print ("[1]:Arp Cache Poisoner Tools(Coming Soon)")
 print ("[2]:Fake Access Point Generator Tools")
-print ("[3]:SSID Hidden Locator Tools")
+print ("[3]:SSID Hidden Locator Tools(Coming Soon)")
 print ("[4]:Hash Generator Tools")
 print ("[5]:SYN Flooder Tools")
 TN = input ("[+]Please Enter the Toools Number: ")
 if TN == 1:
-	os.system("python Arp.py")
+	print "Coming Soon"
+	#os.system("python Arp.py")
 if TN == 2:
 	os.system("python FKAP.py")
 if TN == 3:
-	os.system("python SSID.py")
+	print "Coming Soon"
+	#os.system("python SSID.py")
 if TN == 4:
 	os.system("python Hash.py")
 if TN == 5:
