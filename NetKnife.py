f0248f18f3bfc11c162b5cba238b22b665702798
AmirHoseinTangsiriNET <64230943+AmirHoseinTnagsiriNET@users.noreply.github.com>
2020-06-12 15:46:54 +0430

    Update NetKnife.py

--- NetKnife.py
+++ NetKnife.py
@@ -271,23 +271,23 @@ def TCPPortScanner():
     """
 
 
-scanner = nmap.PortScanner()
+	scanner = nmap.PortScanner()
 
-target = raw_input("Plese Enter The IP Or Domin Target: ")
-portrange = raw_input("Plese Enter The Port Range(For Example 22-443): ")
+	target = raw_input("Plese Enter The IP Or Domin Target: ")
+	portrange = raw_input("Plese Enter The Port Range(For Example 22-443): ")
 
-scanResult = scanner.scan(target, portrange , arguments='-T4 -sV')
+	scanResult = scanner.scan(target, portrange , arguments='-T4 -sV')
 
-for host in scanner.all_hosts():
-    print("Host Address: %s (%s)" % (host, scanner[host].hostname()))
-    for proto in scanner[host].all_protocols():
-        print("Protocol Type: %s" % proto)
+	for host in scanner.all_hosts():
+		print("Host Address: %s (%s)" % (host, scanner[host].hostname()))
+    		for proto in scanner[host].all_protocols():
+        		print("Protocol Type: %s" % proto)
  
-        Port_Number = scanner[host][proto].keys()
-        Port_Number.sort()
-        for port in Port_Number:
-            print ("Port: %s\tstate : %s" % (port, scanner[host][proto][port]['state']))
-print "Aggressive Result %s:" % scanResult
+			        Port_Number = scanner[host][proto].keys()
+			        Port_Number.sort()
+			        for port in Port_Number:
+			            print ("Port: %s\tstate : %s" % (port, scanner[host][proto][port]['state']))
+					print "Aggressive Result %s:" % scanResult
 
 
 	
